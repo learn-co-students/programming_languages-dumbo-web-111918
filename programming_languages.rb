7a9d9709a3048ce6f8c4679f03cb3429b26f64c6
Roman Pirag <mr.anadale@yahoo.com>
2018-11-09 18:14:15 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,17 @@
 def reformat_languages(languages)
-  # your code here
+  new_hash = {}
+  languages[:oo].each do |name, type|
+    new_hash[name] = type 
+    new_hash[name][:style] = [:oo] 
+  end  
+    languages[:functional].each do |name, type|
+      if name == :javascript 
+        new_hash[name] = type
+        new_hash[name][:style] = [:oo, :functional]
+      else
+      new_hash[name] = type 
+      new_hash[name][:style] = [:functional] 
+      end
+  end  
+  return new_hash
 end
