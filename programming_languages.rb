4ba4ef646c04edf2be6fe17ad23370b7ec1abf87
Albina Zakirova <zak1804@gmail.com>
2018-11-18 18:57:20 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,11 @@
-def reformat_languages(languages)
-  # your code here
+require pry
+
+def reformat_languages
+  hash = {}
+  language[style].each do |type, lang|
+    binding.pry
+   #hash[lang] = {style: , type:}
+ 
+   puts hash
+  end
 end
