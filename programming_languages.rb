e5e1ac49675150403897339934cf2aaf33ea5137
Albina Zakirova <zak1804@gmail.com>
2018-11-18 19:17:45 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -3,9 +3,8 @@ require 'pry'
 def reformat_languages(language)
   hash = {}
   language.each do |style, attributes|
-    language[attributes].each do |lang, type|
-       binding.pry
-       
-    end
+    
+    binding.pry
   end
+  
 end
