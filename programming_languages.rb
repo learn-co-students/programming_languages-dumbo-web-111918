70ee712d1dd4e3d92291c474603a2aa10acc831e
Albina Zakirova <zak1804@gmail.com>
2018-11-18 19:12:56 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,11 +1,11 @@
-require pry
+require 'pry'
 
-def reformat_languages
+def reformat_languages(language)
   hash = {}
-  language[style].each do |type, lang|
-    binding.pry
-   #hash[lang] = {style: , type:}
- 
-   puts hash
+  language.each do |style, attributes|
+    language[attributes].each do |lang, type|
+       binding.pry
+       
+    end
   end
 end
